195f2e552ff77835ab2acf2aa38e85e86b97753d
SahilShidruk <SahilShidruk2023@gmail.com>
2025-11-02 23:14:22 +0530

    Add .gitignore and initial DataTypes.cpp implementation

---
+++ C++/Chapter 1/DataTypes.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <iostream>
+
+
+int main() {
+    // Different data types in C++
+
+    // 1. Integer
+    int a = 10;
+    // 2. Character
+    char b = 'A';
+    // 3. Floating point
+    float c = 5.5;
+    // 4. Double 
+    double d = 9.99;
+    // 5. Boolean
+    bool e = true;
+
+    // Output the values
+
+    // 1. Integer
+    // We use %d for integers
+    printf("Integer: %d\n", a);
+    // 2. Character
+    // We use %c for characters
+    printf("Character: %c\n", b);
+    // 3. Floating point
+    // We use %f for floats
+    printf("Float: %.2f\n", c);
+    // 4. Double
+    // We use %lf for doubles
+    printf("Double: %.2lf\n", d);
+    // 5. Boolean
+    // We use %d for booleans
+    printf("Boolean: %d\n", e);
+
+    return 0;
+
+}
+
